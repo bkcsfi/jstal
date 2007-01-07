ea431b4145103429328dee57fec25998c2f38c9e
bkc <bkc@localhost>
2007-01-07 04:10:38 +0000

    snapshot checkin, started on expression handling.

--- jstal.js
+++ jstal.js
@@ -1,10 +1,12 @@
 // jstal.js - javascript implementation of TAL
 // runs on the client
 
-// xmlns:jtal="http://murkworks.com/namespaces/javascripttal"
+// xmlns:jal="http://murkworks.com/namespaces/javascript_tal"
 
-var JAVASCRIPT_TAL_NAMESPACE="http://murkworks.com/namespaces/javascripttal";
+var JAVASCRIPT_TAL_NAMESPACE="http://murkworks.com/namespaces/javascript_tal";
 
+JAVASCRIPT_TAL_NOTHING = new Object();
+JAVASCRIPT_TAL_DEFAULT = new Object();
 
 jsTalTemplate = function(args) {
 	this.template_element = args.template_element;
@@ -12,17 +14,52 @@ jsTalTemplate = function(args) {
 	    throw new TypeError("dom_element must be defined");
 	    
 	this.jstal_namespace = JAVASCRIPT_TAL_NAMESPACE;
+	this.reserved_variable_names = { // templates should not define these
+			'options':true,
+			'nothing':true,
+			'default':true,
+			'repeat':true,
+			'attrs':true,
+		};
+		
+	this.strip_space_re = new RegExp('[^ ]+');	// .match returns a string of text w/o whitespace
 }
 
 jsTalTemplate.prototype = {
 
-	"tal_to_html": function(context) {
+	"to_html": function(options) {
 		// expand the template and
 		// return the expanded results as a string of html
-		// context is an object containing the
-		// root context
+		// options is an object containing the
+		// passed in args, see TALES specification
 		
-		return null;
+		var context = {
+			'options':options
+		};
+		
+		var template = this.compiled_template;
+		var result_html = [];
+		this.html_expand_template(template, context, result_html);
+		
+		return '<b>default return</b>';
+	},
+	
+	"html_expand_template" : function(template, context, result_html) {
+		// expand this template and children, append to result_html
+		var close_tag = null;
+		if(!template.always_omit_tag) {
+			// conditionally omit tag
+			var omit_tag = false;
+			if(template.sometimes_omit_tag) {
+				var omit_tag_expression = (template.tal_attributes['omit-tag'] || {}).expression;
+				if(omit_tag_expression) {
+					var omit_tag = omit_tag_expression(context);
+				}
+			}
+			if(!omit_tag) {
+			
+			}
+		}
 	},
 	
 	"compile": function() {
@@ -81,13 +118,36 @@ jsTalTemplate.prototype = {
 				}
 				
 				element_attributes[namespace_info.local_name] = namespace_info;
-			} else 
-				tal_attributes[namespace_info.local_name] = namespace_info;
+			} else {
+				var local_name = namespace_info.local_name;
+				tal_attributes[local_name] = namespace_info;
+				
+				
+			}
 		}
 		
 		e.element_attributes = element_attributes;
 		e.tal_attributes = tal_attributes;
+		e.sometimes_omit_tag = false;
 		
+		if(tal_attributes['omit-tag'] !== undefined) {
+			e.sometimes_omit_tag = true;
+			if(!tal_attributes['omit-tag'].nodeValue) {
+				// empty string means we will always 
+				// omit the tag, so don't need to test during
+				// expansion
+				e.always_omit_tag = true;
+			} 
+			if(e.declare_namespaces) {
+				// this tag might get left out, so
+				// tell children we're not going to declare
+				// any namespaces
+				for(var i=0, l=e.declare_namespaces.length; i < l; i++) {
+					var namespace = e.declare_namespaces[i];
+					delete parent_namespace_map[namespace[0]];
+				}
+			}
+		}
 		// expand children
 		var childNodes = [];
 		for(var node=element.firstChild; node; node=node.nextSibling) {
@@ -121,7 +181,6 @@ jsTalTemplate.prototype = {
 		// namespace_map is a mapping of namespaces already declared
 		// by a parent node
 	
-		console.debug("node ",node);
 		var local_name = node.localName;
 		var prefix = node.prefix || null;
 		var namespaceURI = node.namespaceURI;
@@ -130,6 +189,48 @@ jsTalTemplate.prototype = {
 			"prefix":prefix,
 			"namespaceURI":namespaceURI
 		}
+	},
+	
+	"compile_path_expression" : function(expression_text) {
+		// generates a function object that evaluates context
+		// and returns a value
+		// TALES spec, path:a/b/c  | nothing
+		// maybe later, allowences for E4X or xpath
+		
+		var terminals = expression_text.split('|');
+		var expressions = [];
+		for(i=0, l=terminals.length; i < l; i++) {
+			var terminal = this.strip_space_re.match(terminals[i]);
+			if(!terminal) {
+				throw new TypeError("missing path text when evaluating path expression:"+expression_text);
+			}
+			expressions.push(terminal);
+		}
+		var function_text = [];
+		
+		for(var i=0, l=expressions.length; i < l; i++) {
+			// every expression starts with the default context
+			var expression = expressions[i];
+			if(expression == 'nothing') {
+				function_text.push('return JAVASCRIPT_TAL_NOTHING;');			
+			} else if(expression == 'default') {
+				function_text.push('return JAVASCRIPT_TAL_DEFAULT;');
+			} else {
+				var steps = expression.split('/');
+				function_text.push('do {');
+				function_text.push('var c = context;'); // establish current context
+				for(var is=0, ls=steps.length; is < ls; is++) {
+					var step = steps[is];
+					function_text.push('var c=c.'+step+';');				
+					function_text.push('if(!c) break;');
+				}
+				function_text.push('} while(false);');
+				function_text.push('if(c) return c;');
+			}
+		}
+		function_text.push('raise new Error("expression evaluation failed: ' + expression + '");');
+		function_text = " ".join(function_text);
+		return new Function('context', function_text);
 	},
 	
 	"copy_object": function(obj) {
