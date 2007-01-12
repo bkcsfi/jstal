875d8858e81299631bd4faf77205957b36bbe279
bkc <bkc@localhost>
2007-01-12 02:42:10 +0000

    Added not: and string expression handling.

--- jstal.js
+++ jstal.js
@@ -35,7 +35,9 @@ jsTalTemplate.prototype = {
 		
 		var context = {
 			'options':options,
-			'repeat':{}
+			'repeat':{},
+			'locals':{},
+			'globals':{},
 		};
 		
 		var template = this.compiled_template;
@@ -54,24 +56,38 @@ jsTalTemplate.prototype = {
 		var node_info = template.node_info;
 		// simple, content  only supported
 		var attrs = '';
+
+		var tagname = this.generate_tagname(node_info);
 		
-		result_html.push('<'+node_info.local_name+attrs + '>');
-		close_tag = "</" +node_info.local_name + ">";
-		
-		var tal_attributes = template.tal_attributes;
-		var tal_content = tal_attributes['content'];
+		result_html.push('<'+tagname+attrs + '>');
+		close_tag = "</" +tagname + ">";
 		var process_child_nodes = true;
-		if(tal_content) {
-			// replace the content of this element
-			// with expression result
-			var content = tal_content.expression(context);
-			if(content !== JAVASCRIPT_TAL_DEFAULT) {
+		
+		try {
+			var tal_attributes = template.tal_attributes;
+			var tal_content = tal_attributes['content'];
+			if(tal_content) {
+				// replace the content of this element
+				// with expression result
+				var content = tal_content.expression(context);
+				if(content !== JAVASCRIPT_TAL_DEFAULT) {
+					process_child_nodes = false;
+					if(content === JAVASCRIPT_TAL_NOTHING)
+						content = '';
+					result_html.push(content);
+				}
+			} 
+		}
+		catch(e) {
+			// an error occured in content, do we have an on-error?
+			if(template.onerror) {
 				process_child_nodes = false;
-				if(content === JAVASCRIPT_TAL_NOTHING)
-					content = '';
-				result_html.push(content);
-			}
-		} 
+				var content = template.onerror(context, e, template);
+				if(content !== JAVASCRIPT_TAL_DEFAULT && content !== JAVASCRIPT_TAL_NOTHING) {
+					result_html.push(content);
+				}
+			} else throw e;
+		}
 		if(process_child_nodes) {
 			var childNodes = template.childNodes;
 	
@@ -88,7 +104,7 @@ jsTalTemplate.prototype = {
 		this.compiled_template = this.compile_element(this.template_element, {});
 	},
 
-	"compile_element" : function(element, parent_namespace_map) {
+	"compile_element" : function(element, parent_namespace_map, on_error_expression) {
 		// compile this element and it's children into
 		// a template and return it
 		
@@ -143,7 +159,7 @@ jsTalTemplate.prototype = {
 				var local_name = node_info.local_name;
 				tal_attributes[local_name] = node_info;
 				
-				this.compile_tal_attribute(node_info);
+				this.compile_tal_attribute(node_info, e.node_info);
 				
 			}
 		}
@@ -152,6 +168,12 @@ jsTalTemplate.prototype = {
 		e.tal_attributes = tal_attributes;
 		e.sometimes_omit_tag = false;
 		
+		var tal_onerror = tal_attributes['on-error'];
+		if(tal_onerror !== undefined && tal_onerror.expression) {
+			e.onerror = on_error_expression = tal_onerror.expression;
+		} else
+			e.onerror = on_error_expression;
+			
 		if(tal_attributes['omit-tag'] !== undefined) {
 			e.sometimes_omit_tag = true;
 			if(!tal_attributes['omit-tag'].nodeValue) {
@@ -175,7 +197,7 @@ jsTalTemplate.prototype = {
 		for(var node=element.firstChild; node; node=node.nextSibling) {
 			if (node.nodeType == 1) {	// element
 				var parent_namespace_map_copy = this.copy_object(parent_namespace_map);
-				childNodes.push(this.compile_element(node, parent_namespace_map_copy));				
+				childNodes.push(this.compile_element(node, parent_namespace_map_copy, on_error_expression));
 			} else if(node.nodeType == 3) { // text node
 				childNodes.push(
 					{
@@ -191,14 +213,32 @@ jsTalTemplate.prototype = {
 		return e;
 	},
 	
-	"compile_tal_attribute" : function(node_info) {
+	"compile_tal_attribute" : function(node_info, parent_node_info) {
 		var nodeValue = node_info.nodeValue;
-		
+		var tagname = this.generate_tagname(parent_node_info);
 		switch(node_info.local_name)  {
 			case "content" :
-				node_info.expression = this.decode_expression(nodeValue);
+				node_info.expression = this.decode_expression(nodeValue,
+											'<'+tagname +" tal:content='"+nodeValue + "' />");
+				break;
+			case "on-error" :
+				node_info.expression = this.decode_expression(nodeValue,
+											'<'+tagname +" tal:on-error='"+nodeValue + "' />");
 				break;
 		}
+	},
+	
+	"trim" : function(s) {
+		// trim leading and trailing whitespace
+		return s.replace(/^\\s+|\\s+$/g,'');
+	},
+	
+	"generate_tagname" : function(node_info) {
+		// return tagname string, possible with xmlns
+		if(node_info.prefix)
+			return node_info.prefix + ':' + node_info.local_name;
+		else
+			return node_info.local_name;
 	},
 	
 	"extract_node_info" : function(node) {
@@ -217,16 +257,68 @@ jsTalTemplate.prototype = {
 		}
 	},
 	
-	"decode_expression" : function(expression_text) {
+	"decode_expression" : function(expression_text, error_hint) {
 		// given expression text, determine which 
 		// type of expression it is and return a function
 		// for that type
 		
+		// expressions could be
+		// string: some string
+		// path: some path
+		// javascript: expression (declares a function whose sole arg is context, must return something)
+		// default expression type is path
+		// not: is a valid prefix
+		
 		// for now, only handle path expression w/o leading path:
-		return this.compile_path_expression(expression_text);
+		expression_text = this.trim(expression_text);
+		var negate_expression_results = false;
+		if(0 == expression_text.indexOf('not:')) {
+			expression_text = this.trim(expression_text.substr(4));
+			negate_expression_results = true;
+		}
+		
+		if(0 == expression_text.indexOf('string:')) {
+			// a string expression
+			expression_text = this.trim(expression_text.substr(7));
+			var expression = this.compile_string_expression(expression_text,error_hint);
+		} else if(0 == expression_text.indexOf('path:')) {
+			// a string expression
+			expression_text = this.trim(expression_text.substr(5));
+			var expression = this.compile_string_expression(expression_text,error_hint);
+		} else {
+			// default to path
+			var expression = this.compile_path_expression(expression_text, error_hint);
+		}
+		if(negate_expression_results) {
+			var negate = function() {
+				var results = expression.apply(this, arguments);
+				if(typeof results == 'function') {
+					throw new Error("expression returned function, which cannot be negated: "+error_hint);
+				}
+				if(results) 
+					return false;
+				else
+					return true;
+			}
+			return negate;
+		} else
+			return expression;
+	},
+
+	"compile_string_expression" : function(expression_text, error_hint) {
+		// generates a function object that returns evaluated string
+		// for now, just return the text
+
+		var function_text = [];
+		function_text.push('return "'+expression_text+'";');
+
+		function_text = function_text.join("\n");
+		console.debug("compile string", expression_text, "to", function_text);
+		return new Function('context', function_text);
+		
 	},
 	
-	"compile_path_expression" : function(expression_text) {
+	"compile_path_expression" : function(expression_text, error_hint) {
 		// generates a function object that evaluates context
 		// and returns a value
 		// TALES spec, path:a/b/c  | nothing
@@ -255,18 +347,48 @@ jsTalTemplate.prototype = {
 				function_text.push('return JAVASCRIPT_TAL_DEFAULT;');
 			} else {
 				var steps = expression.split('/');
+				var start_index = 0;
+				// TODO: in the future, we may wish to optimize
+				// lookups by allowing something like options/x.y.z
+				// where x or y might end up being undefined
+				// to allow this, I'd need to push a try /catch
+				// around the do / while. I'm not doing that right now
+				// because setting up exception handling might have
+				// a performance impact. So, you *can* to options/x.y.z
+				// but if x or y is undefined, you'll get an overall exception
 				function_text.push('do {');
-				function_text.push('var c = context;'); // establish current context
-				for(var is=0, ls=steps.length; is < ls; is++) {
+				
+				// determine if this will be a variable lookup or
+				// a special namespace lookup, such as options or repeat
+				if(steps[0] == 'options') {
+					// lookup in options, not locals or globals
+					function_text.push('var c = context.options;'); // establish current context
+					start_index = 1;	// skip this step
+				} else if(steps[0] == 'repeat') {
+					// lookup in repeat, not locals or globals
+					function_text.push('var c = context.repeat;'); // establish current context
+					start_index = 1;	// skip this step
+				} else {
+					// generic variable, test locals first, then globals
+					function_text.push('if(context.locals.' + steps[0] + ' !== undefined) { var c = context.locals; }'); // establish current context
+					function_text.push('else if(context.globals.' + steps[0] + ' !== undefined) { var c = context.globals; }'); // establish current context
+					function_text.push('else break;');
+				}
+				for(var is=start_index, ls=steps.length; is < ls; is++) {
 					var step = steps[is];
-					function_text.push('c=c.'+step+';');				
-					function_text.push('if(!c) break;');
+					function_text.push('c=c.'+step+';');
+					if(is+1 == ls) {
+						// on the last step, optimize the test
+						function_text.push("if(c !== undefined) return c;");
+					} else
+						function_text.push("if(c === undefined) break;");
 				}
 				function_text.push('} while(false);');
-				function_text.push('if(c) return c;');
 			}
 		}
-		function_text.push('throw new Error("expression evaluation failed: ' + expression + '");');
+		if(!error_hint)
+			error_hint = expression;
+		function_text.push('throw new Error("expression evaluation failed: ' + error_hint + '");');
 		function_text = function_text.join("\n");
 		console.debug("compile ", expression_text, "to", function_text);
 		return new Function('context', function_text);
